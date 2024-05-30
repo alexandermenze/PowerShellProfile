d76f6a4393a1cb4161387bb9f8bb028cb6398e3d
Alexander Menze <a.menze@polipol.de>
2024-05-30 08:14:44 +0200

    Add initial config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+### Fancy meta information
+oh-my-posh init pwsh | Invoke-Expression
+
+### Installed real curl (avoid Invoke-Request)
+Remove-Item alias:curl
+
+### Kubernetes Shortcut 'k'
+Set-Alias -Name k -Value kubectl
+kubectl completion powershell | Out-String | Invoke-Expression
+Register-ArgumentCompleter -CommandName k -ScriptBlock $__kubectlCompleterBlock
+
+### Alias docker to podman (mainly for lazydocker)
+Set-Alias -Name docker -Value podman
+
+
+# Auto complete like linux
+
+### Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+### Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
